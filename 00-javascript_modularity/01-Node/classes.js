d7a09d38c14c2b246a9344fc7e52e04dfa6ad15c
RevMatres <benediktheinen1@gmail.com>
2017-10-24 16:05:46 +0200

    added import-export test files for Node's module-model

---
+++ 00-javascript_modularity/01-Node/classes.js
@@ -0,0 +1,48 @@
+
+//
+//
+//    CLASSES.JS
+//    This contains classes
+//
+//
+
+// =============================================================================
+
+//
+//  Classes
+//
+
+class Person {
+  constructor(name, age, color){
+    this.name = name
+    this.age = age
+    this.color = color
+  }
+  setFruitPreference(arr, ind){
+    this.fruitPreference = arr[ind]
+  }
+}
+
+class Cat {
+  constructor(name, furrColor) {
+    this.name = name
+    this.furrColor = furrColor
+  }
+  purr(n){
+    for(var i=0;i<n;i++){
+      console.log("purrrrrrrrrrrrrrrrrrrrr")
+    }
+  }
+}
+
+// =============================================================================
+
+//
+//  Exports from CLASSES.JS
+//
+
+// export default class Person
+module.exports.Person = Person
+
+// export Cat
+module.exports.Cat = Cat
---
+++ 00-javascript_modularity/01-Node/data.js
@@ -0,0 +1,42 @@
+
+//
+//
+//    DATA.JS
+//    This contains objects, that contain Data
+//
+//
+
+// =============================================================================
+
+//
+//  Imports
+//
+
+// import Person Class
+const { Person } = require("./classes.js")
+
+// =============================================================================
+
+//
+//  Data Objects
+//
+
+// array
+let Fruit = ['banana', 'apple', 'tangerine', 'melon', 'cherry']
+
+// object
+let People = {
+  p1: new Person("George", 21, "blue"),
+  p2: new Person("Dieter", 22, "orange"),
+  p3: new Person("Emily", 23, "a very dark bloody red")
+}
+
+// =============================================================================
+
+//
+//  Exports from DATA.JS
+//
+
+// export { Fruit, People }
+module.exports.Fruit = Fruit
+module.exports.People = People
